a57994d1ed0934ffbe7b67021e0073089788816e
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-10 14:41:41 +0530

    update portfolio structure with routing, styles, and favicon changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+	const [active, setActive] = useState("")
+
+	return (
+		<nav className={`
+			${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20
+			${styles.bgPrimary}
+		`}>
+			<div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+				<Link 
+					to="/"
+					className='flex items-center gap-2'
+					onClick={() => {
+						setActive("")
+						window.scrollTo(0, 0)
+					}}
+				/>
+
+				<img src={logo} alt="logo" className="h-9 w-9 object-contain" />
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
