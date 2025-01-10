d3b49f0d2cbd12ab247aabe1158141244d03fa24
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-10 15:51:11 +0530

    update Hero component with new layout, styling, and content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,23 @@ import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
 	return (
-		<section className="relative w-full h-screen mx-auto"></section>
+		<section className="relative w-full h-screen mx-auto">
+			<div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+				<div className="flex flex-col justify-center items-center mt-5">
+					<div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+					<div className='w-1 sm:h-80 h-40 violet-gradient' />
+				</div>
+
+				<div>
+					<h1 className={`${styles.heroHeadText}`}>Hi, I&apos;m <span className='text-[#915EFF]'>Nivindu</span></h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100 `}>
+						I develop 3D visuals, user interfaces, <br className='sm:block hidden' /> and web applications.
+					</p>
+				</div>
+
+			</div>
+			<ComputersCanvas />
+		</section>
 	)
 }
 
