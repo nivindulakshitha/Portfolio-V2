f00b754d1e56f78599b89db373c324b4163b4d77
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-11 09:58:27 +0530

    add SectionWrapper HOC, integrate react-tilt in About component, and update Hero component for animation fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,53 +1,68 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Title } from 'react-title'
-import { styles } from '../styles'
-import { services } from '../constants'
-import { fadeIn, textVariant } from '../utils/motion'
+/* eslint-disable react/no-unknown-property */
+import React from "react";
+import { Tilt } from 'react-tilt'
+import { motion } from "framer-motion";
 
-const ServiceCard = ({ index, title, icon }) => {
-	return (
-		<Title className="xs:w-[250px] w-full">
-			<motion.div
-				variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-				className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+	<Tilt className='xs:w-[250px] w-full'>
+		<motion.div
+			variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+			className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+		>
+			<div
+				options={{
+					max: 45,
+					scale: 1,
+					speed: 450,
+				}}
+				className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
 			>
-				<div
-					// eslint-disable-next-line react/no-unknown-property
-					options={{ max: 45, scale: 1, speed: 450 }}
-					className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[200px] flex justify-evenly flex-col'
-				>
+				<img
+					src={icon}
+					alt='web-development'
+					className='w-16 h-16 object-contain'
+				/>
 
-					<img src={icon} alt={title} className='w-16 h-16 object-contain' />
-					<h3 className=''>{ title }</h3>
-				</div>
-			</motion.div>
-		</Title>
-	)
-}
+				<h3 className='text-white text-[20px] font-bold text-center'>
+					{title}
+				</h3>
+			</div>
+		</motion.div>
+	</Tilt>
+);
 
 const About = () => {
 	return (
 		<>
 			<motion.div variants={textVariant()}>
-				<p className={`${styles.sectionSubText}`}>Introduction</p>
-				<h2 className={`${styles.sectionHeadText}`}>Overview.</h2>
+				<p className={styles.sectionSubText}>Introduction</p>
+				<h2 className={styles.sectionHeadText}>Overview.</h2>
 			</motion.div>
 
-			<motion.p variants={fadeIn("", "", 0.1, 1)} className='text-secondary font-medium text-[16px] mt-4 max-w-3xl leading-[30px]'>
-				I&apos;m skilled in front-end development, with a strong foundation in web technologies. I have a passion for creating responsive, user-friendly websites and applications. I&apos;m always eager to learn new technologies and improve my skills. I&apos;m a team player and I&apos;vm always ready to collaborate with other developers to create amazing products.
+			<motion.p
+				variants={fadeIn("", "", 0.1, 1)}
+				className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+			>
+				I&apos;m a skilled software developer with experience in TypeScript and
+				JavaScript, and expertise in frameworks like React, Node.js, and
+				Three.js. I&apos;m a quick learner and collaborate closely with clients to
+				create efficient, scalable, and user-friendly solutions that solve
+				real-world problems. Let&apos;s work together to bring your ideas to life!
 			</motion.p>
 
-			<div className="mt-20 flex flex-wrap gap-10">
-				{
-					services.map((service, index) => (
-						<ServiceCard key={index} index={index} {...service} />
-					))
-				}
+			<div className='mt-20 flex flex-wrap gap-10'>
+				{services.map((service, index) => (
+					<ServiceCard key={service.title} index={index} {...service} />
+				))}
 			</div>
 		</>
-	)
-}
+	);
+};
 
-export default About
+export default SectionWrapper(About, "about");
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,10 +3,21 @@ import { motion } from 'framer-motion'
 import { styles } from '../styles'
 import { staggerContainer } from '../utils/motion'
 
-const SectionWrapper = (Component, idname) => {
-	return (
-		<div>SectionWrapper</div>
-	)
-}
+const SectionWrapper = (Component, idname) =>
+	function HOC() {
+		return (
+			<motion.section
+				variants={staggerContainer()}
+				initial="hidden"
+				whileInView="show"
+				className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+				id={idname}
+				viewport={{once: true, amount: 0.33}}
+			>
+				<span className='hash-span' id={idname}>&nbsp;</span>
+				<Component />
+			</motion.section>
+		)
+	}
 
 export default SectionWrapper
