809a65459e8c8f6dd23dfd1cc797594d11b5f09a
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-14 10:51:44 +0530

    refactor Tech component to use SectionWrapper HOC, implement BallCanvas for technology display, and enhance rendering with framer-motion

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants'
+import { div } from 'framer-motion/client'
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
+	return (
+		<div className="flex flex-row flex-wrap justify-center gap-10">
+			{ 
+				technologies.map((tech, index) => {
+					return (
+						<div className="w-28 h-28" key={index}>
+							<BallCanvas tech={tech} icon={tech.icon} />
+						</div>
+					)
+				})
+			}
+		</div>
+	)
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/prop-types */
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
-const Ball = () => {
-  return (
-    <div>Ball</div>
-  )
+const Ball = (props) => {
+	const [decal] = useTexture([props.imgUrl]);
+
+	return (
+		<Float speed={1.75} rotationIntensity={1} floatIntensity={5}>
+			<ambientLight intensity={2} />
+			<directionalLight position={[0, 0, 0.05]} />
+			<mesh castShadow receiveShadow scale={2.75}>
+				<icosahedronGeometry args={[1, 1]} />
+				<meshStandardMaterial
+					color='#DDDDDD'
+					polygonOffset
+					polygonOffsetFactor={-5}
+					flatShading
+				/>
+				<Decal
+					position={[0, 0, 1]}
+					rotation={[2 * Math.PI, 0, 6.25]}
+					scale={1}
+					map={decal}
+					flatShading
+				/>
+			</mesh>
+		</Float>
+	)
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+	return (
+		<Canvas
+			frameloop='always'
+			dpr={[1, 2]}
+			gl={{ preserveDrawingBuffer: true }}
+		>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls enableZoom={false} />
+				<Ball imgUrl={icon} />
+			</Suspense>
+			<Preload all />
+		</Canvas>
+	)
+}
+
+export default BallCanvas
