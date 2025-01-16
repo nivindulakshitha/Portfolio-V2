b28088256b4626eeb088d47c0431f940aca5939f
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-16 22:04:31 +0530

    add Developer component and enhance ProjectCard with image display and GitHub link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+import Developer from './components/Developer'
 
 function App() {
 	return (
@@ -18,6 +19,7 @@ function App() {
 					<Contact />
 					<StarsCanvas />
 				</div>
+				<Developer />
 			</div>
 			</BrowserRouter>
 	)
---
+++ src/components/Developer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Developer = () => {
+	return (
+		<div className="w-full h-fit min-h-52">
+			 
+		</div>
+	)
+}
+
+export default Developer
