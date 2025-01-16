15deea45cacaa9a1bf34665816bbeea2badc0c70
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-16 12:40:03 +0530

    refactor Tech and Works components to improve technology display logic and enhance project presentation with animations

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,14 +3,6 @@ import { BallCanvas } from "./canvas"
 import { SectionWrapper } from '../hoc'
 import { technologies } from '../constants'
 
-const renderingTechnologies = (isMobile) => {
-	if (isMobile) {
-		return technologies.splice(0, 4);
-	}
-
-	return technologies;
-}
-
 const Tech = () => {
 	const isMobile = window.matchMedia("(max-width: 500px)").matches;
 
@@ -18,7 +10,7 @@ const Tech = () => {
 		<div className="flex flex-row flex-wrap justify-center gap-10">
 			{ 
 				//TODO: Currently, last first 5 technologies are displayed. 
-				renderingTechnologies(isMobile).map((tech, index) => {
+				(isMobile ? technologies.slice(5, -1) : technologies).map((tech, index) => {
 					return (
 						<div className="w-28 h-28" key={index}>
 							<BallCanvas tech={tech} icon={tech.icon} />
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { Tilt } from "react-tilt"
+import { motion } from "framer-motion"
+import { SectionWrapper } from "../hoc"
+import { styles } from "../styles"
+import { github } from "../assets"
+import { projects } from "../constants"
+import { fadeIn, textVariant } from "../utils/motion"
 
-const Works = () => {
-  return (
-    <div>Works</div>
-  )
+const ProjectCard = ({ index, name, title, description, tech, link, tags }) => {
+	return (
+		<motion.div variants={fadeIn("up", "sprint", index * 0.5, 0.75)}>
+			
+		</motion.div>
+	)
 }
 
-export default Works
+const Works = () => {
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={styles.sectionSubText}>My works</p>
+				<h2 className={styles.sectionHeadText}>Projects.</h2>
+			</motion.div>
+
+			<div className="w-full flex">
+				<motion.div
+					varients={fadeIn("", "", 0.1, 1)}
+					className="mt-3 text-secondary text-[16px] max-w-3xl leading-[30px]"
+				>
+					Following are some of the projects I have worked on. You can find more of my projects on my GitHub profile.
+				</motion.div>
+			</div>
+
+			<div className="mt-20 flex flex-wrap gap-7">
+				{
+					projects.map((project, index) => {
+						return (
+							<ProjectCard key={index} {...project} index={index} />
+						)
+					})
+				}
+			</div>
+		</>
+	)
+}
+
+export default SectionWrapper(Works, "works")
