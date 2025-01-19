cadbfe8345f0eaf7570779ca8b87d25bf3111f74
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-19 17:26:29 +0530

    refactor: enhance Stars component with canvas rendering and structure for improved visual effects

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { PointMaterial, Points, Preload } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { div } from 'framer-motion/client'
+import React, { Suspense, useRef, useState } from 'react'
+import * as random from 'maath/random/dist/maath-random.esm'
+
 
 const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
+	return (
+		<div>Stars</div>
+	)
+}
+
+const StarsCanvas = () => {
+	return (
+		<div className="w-full h-full absolute inset-0 z-[-1]">
+			<Canvavs>
+
+			</Canvavs>
+		</div>
+	)
 }
 
 export default Stars
