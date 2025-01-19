8c62f844e7fac8263d8605180c5e520d5d2a014d
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-19 17:53:22 +0530

    refactor: optimize Tech component rendering logic and adjust technology display for mobile responsiveness

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,25 +8,16 @@ import { styles } from '../styles'
 
 const Tech = () => {
 	const isMobile = window.matchMedia("(max-width: 500px)").matches;
-	const [showTech, setShowTech] = useState(false); // State to control when to render BallCanvas
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setShowTech(true); // Start rendering after 3 seconds
-		}, 10000);
-
-		return () => clearTimeout(timer); // Cleanup the timer on component unmount
-	}, []);
 
 	return (
 		<>
 			<motion.div variants={textVariant()}>
 				<p className={styles.sectionSubText}>Working with variety of projects, I experienced thse </p>
-				<h2 className={styles.sectionHeadText}>Tech Stack.</h2><br/>
+				<h2 className={styles.sectionHeadText}>Tech Stack.</h2><br />
 			</motion.div>
 			<div className="flex flex-row flex-wrap justify-center gap-10">
-				{showTech &&
-					(isMobile ? technologies.slice(6, -1) : technologies).map((tech, index) => {
+				{
+					(isMobile ? technologies.slice(8, -1) : technologies.slice(1, -1)).map((tech, index) => {
 						return (
 							<div className="w-28 h-28" key={index}>
 								<BallCanvas tech={tech} icon={tech.icon} />
