eb91daf52cba37113be8ff3931fa1e478975bb74
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-20 17:48:34 +0530

    refactor: disable specific ESLint rules for React component properties

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,8 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': 'off',
+      'react/no-unknown-property': 'off',
+      'react/prop-types': 'off',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
