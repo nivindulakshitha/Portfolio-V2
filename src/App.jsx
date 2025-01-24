07f1e720d2a4ac477de50c42a071c1e6f95e6fdc
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-24 07:25:58 +0530

    feat: implement Loading component with progress indication and fade-out effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { BrowserRouter } from 'react-router-dom'
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, ComputersCanvas } from './components'
+import { About, Contact, Experience, Hero, Navbar, Tech, StarsCanvas } from './components'
 import Projects from './components/Projects'
+import Loading from './components/Loading'
+import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 function App() {
+	const [doneLoading, setDoneLoading] = useState(false)
+
 	return (
 		<BrowserRouter>
 			<div className="relative z-0 bg-primary select-none">
+				<AnimatePresence>
+					{!doneLoading && (
+						<motion.div
+							className="bg-hero bg-primary bg-cover bg-no-repeat bg-center z-50 fixed w-screen h-screen"
+							initial={{ opacity: 1 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 1.5 }}
+						>
+							<Loading setDoneLoading={setDoneLoading} />
+						</motion.div>
+					)}
+				</AnimatePresence>
+
+				{/* Main Content */}
 				<div className="bg-hero bg-hero-pattern bg-cover bg-no-repeat bg-center">
 					<Navbar />
 					<Hero />
@@ -13,7 +33,6 @@ function App() {
 				<About />
 				<Tech />
 				<Projects />
-				{/* <Works /> */}
 				<Experience />
 				<div className="relative z-0">
 					<Contact />
