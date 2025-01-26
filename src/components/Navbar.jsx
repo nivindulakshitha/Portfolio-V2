9c1324a7b74b57f56477d1df4a9d99c4cd5fad01
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-26 10:49:01 +0530

    feat: update Navbar component to change gradient color from blue to black

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
 						onClick={() => setToggle(!toggle)}
 					/>
 
-					<div className={`${!toggle ? 'hidden' : 'flex'} p-6 blue-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `}>
+					<div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `}>
 						<ul className="list-none flex sm:hidden justify-end items-start flex-col gap-4">
 							{
 								navLinks.map(link => (
