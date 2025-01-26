0fd693b9e511d4f95efb72217141ff43d9d322df
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-26 08:47:13 +0530

    feat: update version to 1.1.3, enhance Loading component with improved fade-out timing and additional user guidance

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -7,38 +7,38 @@ const Loading = ({ setDoneLoading }) => {
 	const [readyToFade, setReadyToFade] = useState(false)
 
 	useEffect(() => {
-		// Once progress reaches 100, prepare for fade-out
+		
 		if (progress >= 100) {
 			setTimeout(() => {
-				setReadyToFade(true) // Allow animation to start
-			}, 5000) // Add slight delay for polish
+				setReadyToFade(true) 
+			}, 3000) 
 		}
 	}, [progress])
 
 	useEffect(() => {
-		// Notify parent when the fade-out animation is done
+		
 		if (readyToFade) {
 			setTimeout(() => {
 				setDoneLoading(true)
-			}, 1500) // Matches the duration of the fade-out
+			}, 1000) 
 		}
 	}, [readyToFade, setDoneLoading])
 
-	// Variants for the text animations
+	
 	const textVariants = {
 		hidden: { opacity: 0, y: 20 },
 		visible: (i) => ({
 			opacity: 1,
 			y: 0,
 			transition: {
-				delay: i * 0.3, // Delay each line slightly
+				delay: i * 0.3, 
 				duration: 0.6,
 				ease: 'easeInOut',
 			},
 		}),
 	}
 
-	// Variants for the progress bar
+	
 	const progressVariants = {
 		hidden: { opacity: 0, scale: 0.8 },
 		visible: {
@@ -51,7 +51,7 @@ const Loading = ({ setDoneLoading }) => {
 		},
 	}
 
-	// Variants for the image animation
+	
 	const imageVariants = {
 		hidden: { opacity: 0, x: 100 },
 		visible: {
@@ -66,18 +66,18 @@ const Loading = ({ setDoneLoading }) => {
 
 	return (
 		<motion.section
-			className="relative flex sm:flex-row items-end justify-center flex-wrap h-screen w-screen pointer-events-none flex-col"
+			className="relative flex flex-row items-end h-screen w-screen pointer-events-none px-10 justify-center"
 			initial="hidden"
 			animate="visible"
 		>
-			<div className="flex flex-col items-start justify-center h-screen">
+			<div className="flex flex-col justify-center h-screen sm:me-0 md:me-[250px] xl:me-[275px] items-center z-10">
 				{['a bit d!fferent', 'can make a', '<span class="underline">profound</span> impact.'].map(
 					(text, index) => (
 						<motion.p
 							key={index}
 							className="loading-text"
 							variants={textVariants}
-							custom={index} // Pass index for staggered animation
+							custom={index} 
 							dangerouslySetInnerHTML={{ __html: text }}
 						/>
 					)
@@ -98,13 +98,18 @@ const Loading = ({ setDoneLoading }) => {
 						</>
 					)}
 				</motion.p>
+				<motion.div className="max-w-[450px] bg-secondary bg-opacity-10 rounded-xl text-center mt-20 p-3" variants={progressVariants}>
+					<p className="text-white text-[14px] cursor-pointer flex">
+						Note: 3D models use browser&apos;s WebGL API and device GPU processor to render the model. It may take a while to load and it may freeze the browser for a while in some cases. Please be patient. It is recommended to view this website on a desktop or laptop for the best experience.
+					</p>
+				</motion.div>
 			</div>
-			{/* <motion.img
+			<motion.img
+				className="bottom-0 right-0 h-auto w-[555px] absolute max-h-[80%] max-w-[80%] object-contain hidden md:block z-0"
 				src="./models/nivindulakshitha.png"
-				className="bottom-0 right-0 h-auto min-w-[555px] sm:block"
 				alt="model"
 				variants={imageVariants}
-			/> */}
+			/>
 		</motion.section>
 	)
 }
