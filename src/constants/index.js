40759c5dba23baf99acebe1ef673b4d00fa256b9
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-26 07:35:21 +0530

    feat: update experience section and modify logo in Navbar component

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,30 +116,28 @@ const technologies = [
 
 const experiences = [
 	{
-		title: "React.js Developer",
-		company_name: "Starbucks",
+		title: "Open Source Contributor",
+		company_name: "GitHub",
 		icon: starbucks,
 		iconBg: "#383E56",
-		date: "March 2020 - April 2021",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
+		date: "March 2020 - Current",
+		points: [],
 	},
 	{
-		title: "React Native Developer",
-		company_name: "Tesla",
+		title: "Student",
+		company_name: "Univevrity of Sri Jayewardenepura",
+		icon: starbucks,
+		iconBg: "#383E56",
+		date: "March 2020 - Current",
+		points: [],
+	},
+	{
+		title: "Trainee",
+		company_name: "Bank of Ceylon",
 		icon: tesla,
 		iconBg: "#E6DEDD",
 		date: "Jan 2021 - Feb 2022",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
+		points: [],
 	},
 	{
 		title: "Web Developer",
@@ -147,12 +145,7 @@ const experiences = [
 		icon: shopify,
 		iconBg: "#383E56",
 		date: "Jan 2022 - Jan 2023",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
+		points: [],
 	},
 	{
 		title: "Full stack Developer",
@@ -160,12 +153,7 @@ const experiences = [
 		icon: meta,
 		iconBg: "#E6DEDD",
 		date: "Jan 2023 - Present",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
+		points: [],
 	},
 ];
 
