9bed835ef7f9269d037479778204c1d24c6542c6
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2025-01-27 12:01:02 +0530

    feat: update version to 2.6.3, modify layout in Contact component for better responsiveness, and clean up imports in Developer component

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,9 +1,8 @@
 import { OrbitControls, Preload } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React, { Suspense, useEffect, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Loader from './Loader';
 import Model from '../../public/models/Model';
-
 
 const Developer = ({ isContactVisible = false }) => {
     const [gotCatch, setGotCatch] = useState(false);
